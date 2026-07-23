44378c47751d317123fb86a49e631822e9081798
MiguelSorgP <miguelsorgpinheiro.msp@gmail.com>
2026-07-23 14:21:58 -0300

    Calibração dos coeficientes baseando-se em 0707

---
+++ matrizesCalibracao/calibracao_otima_0707.m
@@ -0,0 +1,16 @@
+% --- PARÂMETROS DE CALIBRAÇÃO ÓTIMOS (GERADO AUTOMATICAMENTE) ---
+% Data de geração: 23/07/2026 14:19:37
+% Arquivo de origem: 0707_selecaoManual.csv
+% Erro Médio 3D Inicial: 0.0562 m (5.62 cm)
+% Erro Médio 3D Ótimo:   0.0306 m (3.06 cm)
+% Redução do Erro 3D:    45.51%
+
+% Matriz Intrínseca da Câmera (K)
+K = [
+      877.248819,     0.000000,   651.813793;
+        0.000000,   893.273217,   360.752038;
+        0.000000,     0.000000,     1.000000
+];
+
+% Coeficientes de Distorção da Lente [k1, k2, p1, p2, k3]
+distCoeffs = [0.143860, 0.103800, 0.022275, 0.008428, -1.928206];
