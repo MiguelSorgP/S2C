3b529c121962a88ad1ef94d19854044f583b795e
MiguelSorgP <miguelsorgpinheiro.msp@gmail.com>
2026-07-28 10:51:49 -0300

    Resultados com seleção manual

---
+++ matrizesCalibracao/calibracao_otima_selecaoManual.m
@@ -0,0 +1,16 @@
+% --- PARÂMETROS DE CALIBRAÇÃO ÓTIMOS (GERADO AUTOMATICAMENTE) ---
+% Data de geração: 28/07/2026 10:46:34
+% Arquivo de origem: resultados_ROI_20_07_selecaoManual.csv
+% Erro Médio 3D Inicial: 0.0464 m (4.64 cm)
+% Erro Médio 3D Ótimo:   0.0234 m (2.34 cm)
+% Redução do Erro 3D:    49.44%
+
+% Matriz Intrínseca da Câmera (K)
+K = [
+      929.921921,     0.000000,   654.136865;
+        0.000000,   851.166969,   364.015650;
+        0.000000,     0.000000,     1.000000
+];
+
+% Coeficientes de Distorção da Lente [k1, k2, p1, p2, k3]
+distCoeffs = [0.238269, -0.702027, -0.005318, 0.006165, 0.440434];
